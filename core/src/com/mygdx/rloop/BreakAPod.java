9305d0750a74d3f9409b61125aa26d152a7b3d7e
Paul <paullehenaff@msn.com>
2016-03-31 20:57:54 +0100

    Added fix for desktop

--- core/src/com/mygdx/rloop/BreakAPod.java
+++ core/src/com/mygdx/rloop/BreakAPod.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.mygdx.screen.MenuScreen;
 import com.mygdx.screen.ScreenManager;
 
@@ -14,10 +15,48 @@ public class BreakAPod extends ApplicationAdapter {
 
     public static int WIDTH = 800, HEIGHT = 600;
 	private SpriteBatch batch;
-	
+
+    static public ShaderProgram createDefaultShader() {
+        String vertexShader = "#version 330 core\n"
+                + "in vec4 " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+                + "in vec4 " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
+                + "in vec2 " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
+                + "uniform mat4 u_projTrans;\n" //
+                + "out vec4 v_color;\n" //
+                + "out vec2 v_texCoords;\n" //
+                + "\n" //
+                + "void main()\n" //
+                + "{\n" //
+                + "   v_color = " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
+                + "   v_color.a = v_color.a * (255.0/254.0);\n" //
+                + "   v_texCoords = " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
+                + "   gl_Position =  u_projTrans * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+                + "}\n";
+        String fragmentShader = "#version 330 core\n"
+                + "#ifdef GL_ES\n" //
+                + "#define LOWP lowp\n" //
+                + "precision mediump float;\n" //
+                + "#else\n" //
+                + "#define LOWP \n" //
+                + "#endif\n" //
+                + "in LOWP vec4 v_color;\n" //
+                + "in vec2 v_texCoords;\n" //
+                + "out vec4 fragColor;\n" //
+                + "uniform sampler2D u_texture;\n" //
+                + "void main()\n"//
+                + "{\n" //
+                + "  fragColor = v_color * texture(u_texture, v_texCoords);\n" //
+                + "}";
+
+        ShaderProgram shader = new ShaderProgram(vertexShader, fragmentShader);
+        if (!shader.isCompiled())
+            throw new IllegalArgumentException("Error compiling shader: " + shader.getLog());
+        return shader;
+    }
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		batch = new SpriteBatch(1000, createDefaultShader());
         ScreenManager.setSreen(new MenuScreen());
 	}
 
